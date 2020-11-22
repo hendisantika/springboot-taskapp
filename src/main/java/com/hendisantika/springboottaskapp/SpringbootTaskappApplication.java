334bc2f912f215d830225974da637e58e3430b4e
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-23 05:34:59 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboottaskapp/SpringbootTaskappApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottaskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTaskappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboottaskapp/SpringbootTaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottaskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTaskappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
