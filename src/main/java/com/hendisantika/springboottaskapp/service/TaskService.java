441f219afbf6cd93a642e899cbbb04ff62b4c382
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-23 05:44:20 +0700

    #9 - Add getTasks in Task Service

--- src/main/java/com/hendisantika/springboottaskapp/service/TaskService.java
+++ src/main/java/com/hendisantika/springboottaskapp/service/TaskService.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springboottaskapp.service;
 
+import com.hendisantika.springboottaskapp.model.Task;
 import com.hendisantika.springboottaskapp.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +21,15 @@ import org.springframework.stereotype.Service;
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
+
+    /**
+     * GET all tasks from DB
+     *
+     * @return all tasks from Database
+     */
+    public Set<Task> getTasks() {
+        Set<Task> taskSet = new HashSet<>();
+        taskRepository.findAll().iterator().forEachRemaining(taskSet::add);
+        return taskSet;
+    }
 }
