a5104b8258a35bfb4bb800dac87296eb9c2891e0
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-23 05:51:53 +0700

    #19 - Add displayByStatus in TaskController

--- src/main/java/com/hendisantika/springboottaskapp/controller/TaskController.java
+++ src/main/java/com/hendisantika/springboottaskapp/controller/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashSet;
@@ -38,6 +39,30 @@ public class TaskController {
         //display all Tasks
         Set<Task> tasks = taskService.getTasks();
         model.addAttribute("tasks", tasks);
+
+        Set<Status> statusList = new HashSet<>();
+        Status.stream().forEach(statusList::add);
+        model.addAttribute("statusList", statusList);
+
+        return "index";
+    }
+
+    /**
+     * Shows Tasks by Status
+     *
+     * @param model      contains TaskObject
+     * @param taskStatus may have the values "open/closed/reopened"
+     * @return Set of Tasks with specific status
+     */
+    @GetMapping(value = "/{status}")
+    public String displayByStatus(Model model, @PathVariable("status") String taskStatus) {
+        if (taskStatus.equals(Status.OPEN.getTypeOfStatus())) {
+            model.addAttribute("tasks", taskService.getTasksByStatus(Status.OPEN));
+        } else if (taskStatus.equals(Status.CLOSED.getTypeOfStatus())) {
+            model.addAttribute("tasks", taskService.getTasksByStatus(Status.CLOSED));
+        } else if (taskStatus.equals(Status.REOPENED.getTypeOfStatus())) {
+            model.addAttribute("tasks", taskService.getTasksByStatus(Status.REOPENED));
+        }
 
         Set<Status> statusList = new HashSet<>();
         Status.stream().forEach(statusList::add);
