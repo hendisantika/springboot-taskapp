6efb77e971421e81c901ad5894a952de714b60b9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-23 05:51:21 +0700

    #18 - Add dashboard in TaskController

--- src/main/java/com/hendisantika/springboottaskapp/controller/TaskController.java
+++ src/main/java/com/hendisantika/springboottaskapp/controller/TaskController.java
@@ -1,8 +1,16 @@
 package com.hendisantika.springboottaskapp.controller;
 
+import com.hendisantika.springboottaskapp.model.Status;
+import com.hendisantika.springboottaskapp.model.Task;
 import com.hendisantika.springboottaskapp.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,9 +22,27 @@ import org.springframework.stereotype.Controller;
  * Time: 05.50
  */
 @Controller
+@RequestMapping(value = {"/tasks", "/"})
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
+    /**
+     * GET all tasks from Database
+     *
+     * @return template view for all tasks
+     */
+    @GetMapping
+    public String dashboard(Model model) {
+        //display all Tasks
+        Set<Task> tasks = taskService.getTasks();
+        model.addAttribute("tasks", tasks);
+
+        Set<Status> statusList = new HashSet<>();
+        Status.stream().forEach(statusList::add);
+        model.addAttribute("statusList", statusList);
+
+        return "index";
+    }
 }
